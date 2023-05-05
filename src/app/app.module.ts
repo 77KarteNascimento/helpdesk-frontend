125071f46ab7964ab4a87edf716859e9455987cc
Karte Nascimento <ti.kartenascimento@gmail.com>
2023-05-04 21:04:58 -0400

    Componente Nav Criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 
-
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatPaginatorModule} from '@angular/material/paginator'; 
 import {MatSnackBarModule} from '@angular/material/snack-bar'; 
@@ -19,17 +18,21 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatSelectModule} from '@angular/material/select'; 
-import {MatTableModule} from '@angular/material/table'; 
+import {MatTableModule} from '@angular/material/table';  
 import {MatRadioModule} from '@angular/material/radio'; 
 import {MatInputModule} from '@angular/material/input'; 
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatListModule} from '@angular/material/list'; 
-import {MatCardModule} from '@angular/material/card'; 
+import {MatCardModule} from '@angular/material/card';
+
+//Componentes do Projeto
+import { NavComponent } from './components/nav/nav.component'; 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
